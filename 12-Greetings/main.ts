f826e6437b02096490a80784f195b1fbcf8b24b1
MianMohsinFarrukh <1mohsinfarrukh@gmail.com>
2024-04-11 06:39:32 -0700

    first commit

---
+++ 12-Greetings/main.ts
@@ -0,0 +1,11 @@
+// 12 --- Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+
+
+const names: string[] = ['Ali', 'Asim', 'Kamran', 'Rizwan', 'Faizan'];
+
+let message: string = "Do you like a cricket ?";
+
+
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]+ " " + message);
+}
