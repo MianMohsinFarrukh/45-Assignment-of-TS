c935e4b56fa7ba0a64a7032907c8144f1a98ded1
MianMohsinFarrukh <1mohsinfarrukh@gmail.com>
2024-04-10 22:18:12 -0700

    first commit

---
+++ 09-Favorite Number/main.js
@@ -0,0 +1,5 @@
+"use strict";
+// 09 --- Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+const favNumber = 37; // favorite number in a variable.
+const message = ' My Favourite number is  ' + favNumber; // message that reveals your favorite number.
+console.log(`${message}`);
---
+++ 09-Favorite Number/main.ts
@@ -0,0 +1,10 @@
+// 09 --- Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+
+const favNumber: number = 37;     // favorite number in a variable.
+const message: string = ' My Favourite number is  ' + favNumber; // message that reveals your favorite number.
+
+console.log(`${message}`);
+
+
+
