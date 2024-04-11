b11fa3c94cd76febe7fd87c92cc74185603aa952
MianMohsinFarrukh <1mohsinfarrukh@gmail.com>
2024-04-10 21:51:13 -0700

    first commit

---
+++ 05-Famous Quote-2/main.ts
@@ -0,0 +1,10 @@
+// 05 --- Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+
+
+let author: string = 'Robert Frost';  // name of the famous_person in a variable .
+
+let quote: string = '"Two roads diverged in a wood, and I, I took the one less travelled by, and that has made all the difference."';
+
+console.log(`${author} once said , ${quote}`);
+
+
