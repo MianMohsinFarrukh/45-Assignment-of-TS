03402511bef8317e4b8ccb9b972fef0cc5baa3f3
MianMohsinFarrukh <1mohsinfarrukh@gmail.com>
2024-04-11 06:56:08 -0700

    first commit

---
+++ 13-Your Own Array/main.js
@@ -0,0 +1,4 @@
+"use strict";
+// 13 --- Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+const names = ['Tesla', 'Honda', 'BMW M5', 'Yamaha YZF-R6'];
+names.map((items) => console.log(`I would like to own a ${items}.`));
