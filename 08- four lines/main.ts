25011e499bf784f1c0ab802ccd945be20d685a20
MianMohsinFarrukh <1mohsinfarrukh@gmail.com>
2024-04-10 22:08:01 -0700

    first commit

---
+++ 08- four lines/main.ts
@@ -0,0 +1,26 @@
+// 08 --- You should create four lines that look like this:
+// console.log(5 + 3)
+// Your output should simply be four lines with the number 8 appearing once on each line.
+
+
+let a = 5;
+let b = 3;
+console.log(a + b);            //( Addition ) 
+
+
+let c = 10;
+let d = 2;
+console.log(c - d);           //( Subtraction ) 
+
+let e = 2;
+let f = 4;
+console.log(e * f);          //( multiplication ) 
+
+
+let g = 16;
+let h = 2;
+console.log(g / h);            //( Division  ) 
+
+
+
+
