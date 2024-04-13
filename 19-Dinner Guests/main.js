65591c6d53cc08ae38588994d81978188b3095f8
MianMohsinFarrukh <1mohsinfarrukh@gmail.com>
2024-04-12 22:34:05 -0700

    first commit

--- 19-Dinner Guests/main.js
+++ 19-Dinner Guests/main.js
@@ -1,1 +1,5 @@
 "use strict";
+// 19 --- Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+let invitations = ["Asim", "Ali"];
+let count_invitations = invitations.length;
+console.log(`${count_invitations} people will come to the dinner`);
--- 19-Dinner Guests/main.ts
+++ 19-Dinner Guests/main.ts
@@ -2,17 +2,9 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
+let invitations: string[] = ["Asim", "Ali"];
+let count_invitations: number = invitations.length;
+console.log(`${count_invitations} people will come to the dinner`);
 
 
 
