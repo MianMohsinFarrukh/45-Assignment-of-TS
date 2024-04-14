7f03d8ea5cf2fcb676d098807a3affe0ffa0eca9
MianMohsinFarrukh <1mohsinfarrukh@gmail.com>
2024-04-13 22:27:13 -0700

    first commit

---
+++ 21- could store in a TypeScript Object/main.js
@@ -0,0 +1,9 @@
+"use strict";
+// 21 --- They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+let person = {
+    fName: "mohsin",
+    lName: "farrukh",
+    age: 25,
+    rollNo: 89,
+};
+console.log(person);
---
+++ 21- could store in a TypeScript Object/main.ts
@@ -0,0 +1,14 @@
+// 21 --- They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+
+
+
+let person: { fName: string, lName: string, age: number, rollNo: number } =
+
+{
+    fName: "mohsin",
+    lName: "farrukh",
+    age: 25,
+    rollNo: 89,
+}
+
+console.log(person);
