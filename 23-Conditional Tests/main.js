65fb554c6e15d354bd62d723288e321f800307d4
MianMohsinFarrukh <1mohsinfarrukh@gmail.com>
2024-04-14 07:48:08 -0700

    first commit

---
+++ 23-Conditional Tests/main.js
@@ -0,0 +1,49 @@
+"use strict";
+// 23 --- Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+// let car = 'subaru';
+// console.log("Is car == 'subaru'? I predict True.")
+// console.log(car == 'subaru')
+// • Look closely at your results, and make sure you understand why each line evaluates to True or False.
+// • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+// Test :01 ;
+let car = "subaru";
+console.log("Is car == 'subaru'? I predict True.");
+console.log(car == 'subaru');
+// Test :02 ;
+let myCar = "subaru";
+console.log("Is myCar != 'Toyota'? I predict True.");
+console.log(myCar == 'subaru');
+// Test :03 ;
+let honda = "subaru";
+console.log("Is honda != 'Toyota'? I predict false.");
+console.log(honda == 'subaru');
+// Test :04 ;
+let capital = "SUBARU";
+console.log("Is capital == 'Toyota'? I predict True.");
+console.log(capital != 'subaru');
+// Test :05 ;
+let newItem = "Honda";
+console.log("Is newItem == 'Toyota'? I predict True.");
+console.log(newItem == 'Honda');
+// Test :06 ;
+let carLength = "Honda";
+console.log("Is carLength.length == 8? I predict True.");
+console.log(carLength.length == 6);
+// Test :07 ;
+let newCar = "Honda";
+console.log("Is newCar == 'Toyota'? I predict True.");
+console.log(newCar == "Toyota");
+// Test :08 ;
+let oldCar = "subaru";
+console.log("Is oldCar == 'Toyota'? I predict True.");
+console.log(oldCar == "Toyota");
+// Test :09 ;
+let n1 = 25;
+let n2 = 27;
+console.log("Is n1 : '25' == n2 : '27' ? I predict True.");
+console.log(n1 == n2);
+// Test : 10 ;
+let no1 = 50;
+let no2 = 50;
+console.log("Is no1 : '50' !== no2 : '50' ? I predict True.");
+console.log(no1 !== no2);
---
+++ 24-More Conditional Tests/main.ts
@@ -0,0 +1,131 @@
+// 24 --- More Conditional Tests: You don’t have to limit the number of tests you create to 10. If you want to try more comparisons, write more tests. Have at least one True and one False result for each of the following:
+// 1• Tests for equality and inequality with strings
+
+// 2• Tests using the lower case function
+
+// 3• Numerical tests involving equality and inequality, greater than and less than, greater than or equal to, and less than or equal to
+
+// 4• Tests using "and" and "or" operators
+
+// 5• Test whether an item is in a array
+
+// 6• Test whether an item is not in a array
+
+
+
+
+
+
+
+
+
+// 1• Tests for equality and inequality with strings :
+// inequality strings :
+
+let name_1: string = "Mohsin";
+let name_2: string = "Asim";
+let name_3: string = "Ali";
+
+if (name_1 === name_2) { console.log(`Names are equal `) }
+
+else {
+    console.log(`Names are  not equal `)
+};
+
+
+//equality strings :
+
+if (name_1 != name_3) {
+    console.log(`Names are equal `)
+}
+else {
+    console.log(`Names are  not equal `)
+};
+
+
+
+
+
+
+
+
+
+// 2• Tests using the lower case function :
+
+let string3: string = "HELLO";
+let string4: string = "hello";
+
+// Lowercase test
+console.log(string3.toLowerCase() === string4);
+
+
+
+
+
+
+
+
+
+
+// 3• Numerical tests involving equality and inequality, greater than and less than, greater than or equal to, and less than or equal to :
+
+let num1: number = 25;
+let num2: number = 50;
+
+// Equality and inequality tests :
+console.log(num1 === num2);
+console.log(num1 !== num2);
+
+// Greater than and less than tests :
+console.log(num1 > num2);
+console.log(num1 < num2);
+
+// Greater than or equal to and less than or equal to tests :
+console.log(num1 >= num2);
+console.log(num1 <= num2);
+
+
+
+
+
+
+// 4• Tests using "and" and "or" operators
+
+let a: number = 5;
+let b: number = 10;
+let c: number = 15;
+
+// "and" operator test
+console.log(a < b && b < c);
+
+// "or" operator test
+console.log(a < b || a > c);
+
+
+
+
+
+
+
+// 5• Test whether an item is in a array :
+
+let fruits: string[] = ["apple", "banana", "orange"];
+
+// In array test
+console.log(fruits.includes("banana"));
+
+
+
+
+
+
+
+
+
+
+// 6• Test whether an item is not in a array:
+
+let fruit: string[] = ["apple", "banana", "orange"];
+
+// Not in array test
+console.log(!fruit.includes("grape")); 
