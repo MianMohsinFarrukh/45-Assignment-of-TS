9ee186ce2597719a7a63373069ae0601c8225559
MianMohsinFarrukh <1mohsinfarrukh@gmail.com>
2024-04-14 22:27:48 -0700

    first commit

---
+++ 26-Alien Colors #2/main.ts
@@ -0,0 +1,34 @@
+// 26 --- Alien Colors #2: Choose a color for an alien as you did in Exercise 25, and write an if-else chain.
+// 1• If the alien’s color is green, print a statement that the player just earned 5 points for shooting the alien.
+
+// 2• If the alien’s color isn’t green, print a statement that the player just earned 10 points.
+
+// • Write one version of this program that runs the if block and another that runs the else block.
+
+
+
+
+// 1• If the alien’s color is green, print a statement that the player just earned 5 points for shooting the alien.
+
+let alien_color: string = "green";
+if (alien_color === "green") {
+    console.log("Congratulations! You just earned 5 points for shooting the alien.")
+} else {
+    console.log("Congratulations! You just earned 10 points.")
+};
+
+
+
+
+
+
+
+
+// 2• If the alien’s color isn’t green, print a statement that the player just earned 10 points.
+
+let alien: string = "red";
+if (alien === "green") {
+    console.log("Congratulations! You just earned 5 points for shooting the alien.")
+} else {
+    console.log("Congratulations! You just earned 10 points.")
+};
