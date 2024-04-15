730837303695aaa14910bbb81039c505550ba33e
MianMohsinFarrukh <1mohsinfarrukh@gmail.com>
2024-04-15 00:34:22 -0700

    first commit

---
+++ 31-No Users/main.ts
@@ -0,0 +1,58 @@
+// 31 --- No Users: Add an if test to Exercise 28 to make sure the list of users is not empty.
+// 1• If the list is empty, print the message We need to find some users!
+
+// 2• Remove all of the usernames from your array, and make sure the correct message is printed.
+
+
+
+
+
+
+
+
+
+// 1• If the list is empty, print the message We need to find some users!
+
+
+let userNames: string[] = [];
+
+if (userNames.length === 0) {
+    console.log("We need to find some users!");
+} else {
+    for (let userName of userNames) {
+        if (userName === "admin") {
+            console.log("Hello admin, would you like to see a status report?");
+        } else {
+            console.log(`Hello ${userName}, thank you for logging in again.`);
+        }
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 2• Remove all of the usernames from your array, and make sure the correct message is printed.
+
+
+let myName: string[] = ["Admin", "Mohsin", "Asim", "Ali", "Jawad"];
+
+if (myName.length === 0) {
+    console.log("We need to find some users!");
+} else {
+    for (let yourName of myName) {
+        if (yourName === "admin") {
+            console.log("Hello admin, would you like to see a status report?");
+        } else {
+            console.log(`Hello ${yourName}, thank you for logging in again.`);
+        }
+    }
+};
