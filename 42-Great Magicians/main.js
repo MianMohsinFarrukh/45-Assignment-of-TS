3525f7cb53b2ef5bb7c22a3d68ac46229856812a
MianMohsinFarrukh <1mohsinfarrukh@gmail.com>
2024-04-15 06:17:30 -0700

    first commit

---
+++ 42-Great Magicians/main.js
@@ -0,0 +1,17 @@
+"use strict";
+//  42 --- Great Magicians: Start with a copy of your program from Exercise 39. Write a function called make_great() that modifies the array of magicians by adding the phrase the Great to each magician’s name. Call show_magicians() to see that the list has actually been modified.
+function make_great(magicians) {
+    return magicians.map(magician => `the Great : ${magician}`);
+}
+// Function to print the magician's names
+function show_magicians(magicians) {
+    for (let magician of magicians) {
+        console.log(magician);
+    }
+}
+// Array of magician's names
+let magician_names = ["Mohsin", "Asim", "Penn Jawad", "Faizan"];
+// Call make_great() to modify the array
+magician_names = make_great(magician_names);
+// Call show_magicians() to see the modified list
+show_magicians(magician_names);
