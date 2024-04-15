c34abd0810f630c7ac6c4350a083f37a3ac5d392
MianMohsinFarrukh <1mohsinfarrukh@gmail.com>
2024-04-15 05:47:34 -0700

    first commit

---
+++ 37-Large Shirts/main.js
@@ -0,0 +1,11 @@
+"use strict";
+// 37 --- Large Shirts: Modify the make_shirt() function so that shirts are large by default with a message that reads I love TypeScript. Make a large shirt and a medium shirt with the default message, and a shirt of any size with a different message.
+function make_shirt(size = "large", message = "I love TypeScript") {
+    console.log(`You have ordered a ${size}-sized shirt with the message: "${message}".`);
+}
+// Making a large shirt with the default message
+make_shirt();
+// Making a medium shirt with the default message
+make_shirt("medium");
+// Making a shirt of any size with a different message
+make_shirt("small", "Hello, World!");
